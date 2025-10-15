40357219d69f5c7d1180bb96b1212acae143ed40
Dilsha21 <dilshaindeewari@gmail.com>
2025-10-15 11:15:59 +0530

    Added images to the product table

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 
+app.use('/Assets', express.static(__dirname + '/Assets'));
+
+
 // CORRECTED: The default express.json() line was removed.
 // These two lines now correctly set the size limit for all requests.
 app.use(express.json({ limit: '50mb' }));
